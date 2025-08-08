ff300f4f14bc25c2d84279bead133431b9a798c0
grandma2368 <46317358+grandma2368@users.noreply.github.com>
2025-08-08 13:03:46 +0200

    rest-room-oz : env variables

--- src/components_react/Bartender/Contact.jsx
+++ src/components_react/Bartender/Contact.jsx
@@ -1,4 +1,5 @@
-import { useContext } from "react"
+import { useContext, useRef, useState } from "react"
+import emailjs from "@emailjs/browser"
 
 // Context
 import { AppContext, AppSetterContext } from "../../context/AppContext"
@@ -8,6 +9,34 @@ export default function BarContact()
     // Get Context
     const app = useContext(AppContext)
     const setApp = useContext(AppSetterContext)
+
+    // Email
+    const formRef = useRef()
+    const [messageSent, setMessageSent] = useState(false)
+
+    const contact = (e) => {
+        e.preventDefault()
+
+        // console.log(formRef.current)
+
+        emailjs
+        .sendForm(
+            VITE_EMAILJS_SERVICE_ID,
+            VITE_EMAILJS_TEMPLATE_BARTENDER_ID,
+            formRef.current,
+            { publicKey: VITE_EMAILJS_PUBLIC_KEY }
+        )
+        .then(
+            () => {
+                console.log("SUCCESS")
+                setMessageSent(true)
+                setTimeout(() => { setMessageSent(false) }, 2000)
+            },
+            (error) => {
+                console.log("FAILED : " + error.text)
+            }
+        )
+    }
 
     const close = (event) => {
         if (app.focus === "Barrel") {
@@ -20,7 +49,22 @@ export default function BarContact()
                     <div className="close">
                         <img src="/svg/cross-circle.svg" alt="cross" onClick={ close }/>
                     </div>
-                    CONTACT
+                    <form className="contact-form" ref={ formRef } onSubmit={ contact }>
+                        <div className="datum">
+                            <label>Name</label>
+                            <input type="text" name="user_name" />
+                        </div>
+                        <div className="datum">
+                            <label>Email</label>
+                            <input type="email" name="user_email" />
+                        </div>
+                        <div className="datum">
+                            <label>Message</label>
+                            <textarea name="message" />
+                        </div>
+                        <button type="submit">Contact</button>
+                        <div className={ messageSent ? "message-sent" : "waiting"}>Message Sent !</div>
+                    </form>
                 </div>
             </section>
 }
--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,61 @@
 import react from "@vitejs/plugin-react"
-import { transformWithEsbuild } from "vite"
+import { defineConfig, loadEnv, transformWithEsbuild } from "vite"
 import restart from "vite-plugin-restart"
 import glsl from "vite-plugin-glsl"
 
-export default {
-    root: "src/",
-    publicDir: "../public/",
-    base: "./",
-    plugins:
-    [
-        // Restart server on static/public file change
-        restart({ restart: [ "../public/**", ] }),
+export default defineConfig(({ mode }) => {
 
-        // React support
-        react(),
+    const env = loadEnv(mode, process.cwd(), '')
 
-        // GLSL support
-        glsl(),
-
-        // .js file support as if it was JSX
-        {
-            name: "load+transform-js-files-as-jsx",
-            async transform(code, id)
-            {
-                if (!id.match(/src\/.*\.js$/))
-                    return null
-
-                return transformWithEsbuild(code, id, {
-                    loader: "jsx",
-                    jsx: "automatic",
-                });
-            },
+    return {
+        root: "src/",
+        publicDir: "../public/",
+        base: "./",
+        define: {
+            "VITE_EMAILJS_SERVICE_ID": JSON.stringify(env.VITE_EMAILJS_SERVICE_ID),
+            "VITE_EMAILJS_TEMPLATE_BARTENDER_ID": JSON.stringify(env.VITE_EMAILJS_TEMPLATE_BARTENDER_ID),
+            "VITE_EMAILJS_TEMPLATE_GAME_MASTER_ID": JSON.stringify(env.VITE_EMAILJS_TEMPLATE_GAME_MASTER_ID),
+            "VITE_EMAILJS_PUBLIC_KEY": JSON.stringify(env.VITE_EMAILJS_PUBLIC_KEY)
         },
-    ],
-    server:
-    {
-        host: true, // Open to local network and display URL
-        open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env) // Open if it"s not a CodeSandbox
-    },
-    build:
-    {
-        outDir: "../dist", // Output in the dist/ folder
-        emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
-    },
-}
+        plugins:
+        [
+            // Restart server on static/public file change
+            restart({ restart: [ "../public/**", ] }),
+
+            // React support
+            react(),
+
+            // GLSL support
+            glsl(),
+
+            // .js file support as if it was JSX
+            {
+                name: "load+transform-js-files-as-jsx",
+                async transform(code, id)
+                {
+                    if (!id.match(/src\/.*\.js$/))
+                        return null
+
+                    return transformWithEsbuild(code, id, {
+                        loader: "jsx",
+                        jsx: "automatic",
+                    });
+                },
+            },
+        ],
+        server:
+        {
+            host: true, // Open to local network and display URL
+            open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env) // Open if it"s not a CodeSandbox
+        },
+        build:
+        {
+            outDir: "../dist", // Output in the dist/ folder
+            emptyOutDir: true, // Empty the folder first
+            sourcemap: true // Add sourcemap
+        }
+    }
+})
+
+// export default {,
+// }
