31aed34d736e2ceea10100e9390755548ff8378a
grandma2368 <46317358+grandma2368@users.noreply.github.com>
2025-08-16 14:08:07 +0200

    rest-room-oz : safari WIP

--- src/index.jsx
+++ src/index.jsx
@@ -10,6 +10,18 @@ import Container from "./components_r3f/Container"
 // React Component
 import HtmlExperience from "./components_react/HtmlExperience"
 
+function setRealHeight() {
+    // calcul : 1vh in real pixel
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+
+// Init
+setRealHeight();
+
+// On recalcule à chaque resize/orientationchange
+window.addEventListener("resize", setRealHeight);
+window.addEventListener("orientationchange", setRealHeight);
 
 const root = ReactDOM.createRoot(document.querySelector("#root"))
 
